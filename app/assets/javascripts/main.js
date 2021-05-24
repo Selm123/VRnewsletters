5bcfb7a84d64f7ba7b91dec9c6a649e76b27d8c8
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-24 23:10:23 +1000

    updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('hello');
+$(document).ready(function () {
+  console.log("hello!");
+
+  const show = (element) => {
+    element.addClass("show");
+  };
+
+  $(".close").on("click", function () {
+    $(".pop-up").removeClass("show");
+  });
+
+  $(".sing-up-btn").on("click", function () {
+    $(".pop-up").addClass("show");
+  });
+
+  
+  // $(".header-name").text("show!");
+});
