a0b58a648a5b0a0282b5da5bfd6afb95a074110c
Priyanka Patel <priyanka.syd@gmail.com>
2021-05-24 14:48:44 +1000

    validate

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,5 @@
 class Subscriber < ApplicationRecord
+  validates :email, :presence => true, :uniqueness => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
 end
